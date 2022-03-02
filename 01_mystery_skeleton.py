ca183ddd77f52e24bbf26eefad6ee4dcac7c73b1
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2022-03-03 10:59:50 +1300

    vid 26, game gui

---
+++ 01_mystery_skeleton.py
@@ -0,0 +1,69 @@
+from tkinter import *
+import random
+
+
+class Start:
+    def __init__(self, parent):
+
+        # GUI to get starting balance and stakes
+        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame.grid()
+
+        # Mystery heading (row 0)
+        self.mystery_box_label = Label(self.start_frame, text="Mystery Box Game", font="Arial 19 bold")
+        self.mystery_box_label.grid(row=1)
+
+        # Entry box (row 1)
+        self.start_amount_entry = Entry(self.start_frame, font="Arial 16 bold")
+        self.start_amount_entry.grid(row=2)
+
+        # play button
+        self.lowstakes_button = Button(text="Low ($5)", command=lambda: self.to_game(1))
+        self.lowstakes_button.grid(row=2, pady=10)
+
+    def to_game(self, stakes):
+        starting_balance = self.start_amount_entry.get()
+        Game(self, stakes, starting_balance)
+
+class Game:
+    def __init__(self, partner, stakes, starting_balance):
+        print(stakes)
+        print(starting_balance)
+
+        partner.lowstakes_button.config(state=DISABLED)
+
+        # initialise variables
+        self.balance = IntVar()
+
+        # set starting balance to amount entered by user at start of game
+        self.balance.set(starting_balance)
+        
+        # GUI setup
+        self.game_box = Toplevel()
+
+        # GUI to get starting balance and stakes
+        self.game_frame = Frame(self.game_box)
+        self.game_frame.grid()
+
+        # heading row (row 0)
+        self.game_label = Label(self.game_frame, text="Heading", font="Arial 24 bold", padx=10, pady=10)
+        self.game_label.grid(row=0)
+
+        # Balance label (row 1)
+        self.balance_frame = Frame(self.game_frame)
+        self.balance_frame.grid(row=1)
+
+        self.balance_label = Label(self.game_frame, font="Arial 16 bold", text="Balance: {}".format(starting_balance))
+        self.balance_label.grid(row=3)
+
+        # play button
+        self.play_button = Button(self.game_frame, text="Gain", padx=10, pady=10, command=self.reveal_boxes)
+        self.play_button.grid(row=4)
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("title goes here")
+    something = Start(root)
+    root.mainloop()
