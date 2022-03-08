bcec1ee6909f3d480aae82753e3d99499ec88d39
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2022-03-08 13:17:39 +1300

    vid 31

---
+++ 03_game_GUI_v1.py
@@ -0,0 +1,80 @@
+from tkinter import *
+import random
+
+
+
+class Start:
+    def __init__(self, parent):
+
+        # GUI to get starting balance and stakes
+        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame.grid()
+
+        self.push_me_button = Button(text="Push Me", command=self.to_game)
+        self.push_me_button.grid(row=0, pady=10)
+
+    def to_game(self, stakes):
+
+        # retrieve starting balance
+        starting_balance = self.start_amount_entry.get()
+                
+        Game(self, stakes, starting_balance)
+
+        # hide start up window
+        root.withdraw()
+         
+class Game:
+    def __init__(self, partner, stakes, starting_balance):
+        print(stakes)
+        print(starting_balance)
+
+        # initialise variables
+        self.balance = IntVar()
+
+        # set starting balance to amount entered by user at start of game
+        self.balance.set(starting_balance)
+        
+        # GUI setup
+        self.game_box = Toplevel()
+
+        # GUI to get starting balance and stakes
+        self.game_frame = Frame(self.game_box)
+        self.game_frame.grid()
+
+        # heading row (row 0)
+        self.game_label = Label(self.game_frame, text="Heading", font="Arial 24 bold", padx=10, pady=10)
+        self.game_label.grid(row=0)
+
+        # Balance label (row 1)
+        self.balance_frame = Frame(self.game_frame)
+        self.balance_frame.grid(row=1)
+
+        self.balance_label = Label(self.game_frame, font="Arial 16 bold", text="Balance: {}".format(starting_balance))
+        self.balance_label.grid(row=3)
+
+        # play button
+        self.play_button = Button(self.game_frame, text="Gain", padx=10, pady=10, command=self.reveal_boxes)
+        self.play_button.grid(row=4)
+
+    def reveal_boxes(self):
+        # retrieve the balance from the initial function
+        current_balance = self.balance.get()
+
+        # adjust the balance (subtract game cost and add pay out)
+        # for testing purposes, just add 2
+        current_balance += 2
+
+        # set balance to adjusted balance
+        self.balance.set(current_balance)
+
+        # edit label so user can see their balance
+        self.balance_label.configure(text="Balance: {}".format(current_balance))
+
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("title goes here")
+    something = Start(root)
+    root.mainloop()
