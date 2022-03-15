4eb3a45195147bba5487e5df69d106751227e15c
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2022-03-16 11:34:58 +1300

    generate matrix of buttons (2x2) and show values of buttons. Will be useful for multiple choice questions in next assignment

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -5,26 +5,22 @@ LeftFrame = tk.Frame(root)
 LeftFrame.grid()
 
 def checker(i,j):
-    print("You pressed button {i},{j}")
+    print("You pressed button {},{}".format(i, j))
 
 #Create a 2-d list containing 3 rows, 3 columns (using list comprehension)
 botones = [[None for i in range(3)] for j in range(3) ]
 
-for i in range(3):
-    for j in range(3):
-        a = 1
-        b = 1
+for i in range(2):
+    for j in range(2):
         current_button = tk.Button(LeftFrame,
-                               text = "{},{}".format(a, b),
+                               text = "{}, {}".format(i, j),
                                font=("tahoma", 25, "bold"),
                                height = 3,
-                               width = 8,
+                               width = 20,
                                bg="gainsboro",
                                command=lambda i=i,j=j:checker(i,j)) #lambda is passed parameters i and j
         #Grid occurs on a new line
-        a += 1
-        b += 1
-        current_button.grid(row = i+1, column = j+1)
+        current_button.grid(row = i+1, column = j+1, padx=10, pady=10)
         botones[i][j] = current_button
 
 root.mainloop()
