f4a987be16132a6c16e3ead8306770c869f5bbb2
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2022-03-21 14:32:08 +1300

    added help and integrated working game

--- AA_compiled_game_v1.py
+++ AA_compiled_game_v1.py
@@ -1,6 +1,6 @@
 from tkinter import *
 import random
-
+from functools import partial
 
 
 class Start:
@@ -59,8 +59,12 @@ class Start:
         self.high_stakes_button.config(state=DISABLED)
 
         # instructions / help button
-        self.help_button = Button(self.start_frame, text="How to Play", bg="#808080", fg="white", font=button_font)
+        self.help_button = Button(self.start_frame, text="How to Play", bg="#808080", fg="white", font=button_font, command=self.to_help)
         self.help_button.grid(row=5, pady=10)
+
+    def to_help(self):
+        get_help = Help(self)
+        root.withdraw()
 
     def check_funds(self):
         starting_balance = self.start_amount_entry.get()
@@ -124,8 +128,43 @@ class Start:
         Game(self, stakes, starting_balance)
 
         # hide start up window
-        self.start_frame.withdraw()
-         
+        root.withdraw()
+
+class Help:
+    def __init__(self, partner):
+
+        # disable help button
+        partner.help_button.config(state=DISABLED)
+
+        # sets up child window (ie: help box)
+        self.help_box = Toplevel()
+
+        # if user presses cross at top, closes help and releases help button
+        self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
+
+        # set up GUI frame
+        self.help_frame = Frame(self.help_box, width=300)
+        self.help_frame.grid()
+
+        # set up help heading
+        self.how_heading = Label(self.help_frame, text="Help / Instructions", font="arial 14 bold")
+        self.how_heading.grid(row=0)
+
+        help_text = "Choose an amount to play with and then choose the stakes. Higher stakes cost more per round but you can win more as well.\n\nWhen you enter the play area, you will see three mystery boxes. To reveal the contents of the boxes, click the 'Open Boxes' button. If you don't have enough money to play, the button will turn red and you will need to quite the game.\n\nThe contents of the boxes will be added to your balance. The boxes could contain...\n\nLow: Lead ($0) | Copper ($1) | Silver ($2) | Gold ($10)\nMedium: Lead ($0) | Copper ($2) | Silver ($4) | Gold ($25)\nHigh: Lead ($0) | Copper ($5) | Silver ($10) | Gold ($25)\n\nIf each box contains gold, you earn $30 (low stakes). If they contained copper, silver and gold, you would receive $13 ($1 + $2 + $10) and so on."
+        
+        self.help_text = Label(self.help_frame, text=help_text, justify=LEFT, wrap=400, padx=10, pady=10)
+        self.help_text.grid(row=1)
+
+        # dismiss button (row 2)
+        self.dismiss_btn = Button(self.help_frame, text="Dismiss", width=10, bg="#660000", fg="white", font="arial 15 bold", command = partial(self.close_help, partner))
+        self.dismiss_btn.grid(row=2, pady=10)
+
+    def close_help(self, partner):
+        self.help_box.destroy()
+        partner.help_button.config(state=NORMAL)
+        root.deiconify()
+        
+
 class Game:
     def __init__(self, partner, stakes, starting_balance):
         print(stakes)
